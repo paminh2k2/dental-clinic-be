1ddac81f58c4415d98525d51fc58a224bf15b66d
paminh2k2 <aminh252781@gmail.com>
2024-11-14 11:16:28 +0700

    init commit

---
+++ src/main/java/com/example/dental_clinic_be/DentalClinicBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dental_clinic_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalClinicBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dental_clinic_be/DentalClinicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dental_clinic_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
